e0d7dea77ae9cf42529ab6f4e532fa371b7bb771
Sphynx <2229860255@qq.com>
2018-05-14 11:09:13 +0800

    删除日志输出，失败断言。

--- AirMonitor/Core/Logger.cs
+++ AirMonitor/Core/Logger.cs
@@ -20,11 +20,9 @@ namespace AirMonitor.Core
             Error = 4,
         }
 
-
         public void Error(Exception exception)
         {
-            Trace.Fail($"[{LogType.Error}]{exception}");
-            Trace.TraceError(exception.Message, exception.ToString());
+            Trace.TraceError($"[{LogType.Error}]{exception}");
         }
 
         public void Info(string format, params object[] args)
