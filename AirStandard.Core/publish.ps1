02db6274dbe20dbefd3309703662edf9c2a1304d
张东奇 <zdqa123456@hotmail.com>
2018-07-11 10:43:25 +0800

    发布到arm

---
+++ AirStandard.Core/publish.ps1
@@ -0,0 +1,17 @@
+ #@echo off
+
+#rem dotnet publish -r linux-arm
+#rem cd .\bin\Debug\netcoreapp2.0\linux-arm\publish
+#rem echo open 192.168.1.180 >ftp.up
+#rem echo ftpadmin>>ftp.up
+#rem echo 123456>>ftp.up
+#
+#
+#for /D %i% in * do echo %i% 
+#
+#rem cd ..
+#rem echo -s:ftp.up -i>>upload.log
+#rem del ftp.up /q
+#rem 
+
+
